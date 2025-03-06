0dead79c97df1ff708f1c0ef8f8b8277ab75ece3
kaushikkts <105159350+kaushikkts@users.noreply.github.com>
2025-03-06 14:39:29 +0530

    Add delimiter

---
+++ index.js
@@ -0,0 +1,19 @@
+
+
+const addStringNumbers = (stringNums) => {
+    // even if no input is passed, we will return 0
+    if (!stringNums?.trim()) return 0;
+    let delimiter = /[,\n]+/;
+    if (stringNums.startsWith('//')) {
+        // This means that we are changing the delimiter
+        const delimiterEnd = stringNums.indexOf('\n');
+        // because the startsWith has 2 characters, we need to start the substring
+        // at character 3 which is (2) as it is 0 indexed
+        delimiter = stringNums.substring(2, delimiterEnd);
+        stringNums = stringNums.substring(delimiterEnd + 1);
+
+    }
+
+}
+
+module.exports = addStringNumbers;
