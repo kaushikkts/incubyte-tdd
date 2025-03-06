be29b3d4e0471fc7f756f6d13eb0c61cdfe8ed7a
kaushikkts <105159350+kaushikkts@users.noreply.github.com>
2025-03-06 14:44:50 +0530

    Adding test for negative numbers

--- index.test.js
+++ index.test.js
@@ -9,4 +9,12 @@ describe('String calculator', () => {
     test('should handle single delimiter change', () => {
         expect(addStringNumbers('//;\n1;2')).toStrictEqual(3);
     });
+
+    test('should throw Error on negative number', () => {
+        expect(() => addStringNumbers('//;\n1;-2')).toThrow('Negative numbers not allowed: -2');
+    });
+
+    test('should throw Error on multiple negative numbers and display all of them', () => {
+        expect(() => addStringNumbers('//;\n1;-2;-3;-4')).toThrow('Negative numbers not allowed: -2, -3, -4');
+    });
 })
