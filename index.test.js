07bc0638f78dcfa226319c418b09154ea34648f7
kaushikkts <105159350+kaushikkts@users.noreply.github.com>
2025-03-06 14:44:30 +0530

    Adding test for handling delimiter

--- index.test.js
+++ index.test.js
@@ -5,4 +5,8 @@ describe('String calculator', () => {
     test('should handle no input passed', () => {
         expect(addStringNumbers()).toBe(0);
     });
+
+    test('should handle single delimiter change', () => {
+        expect(addStringNumbers('//;\n1;2')).toStrictEqual(3);
+    });
 })
